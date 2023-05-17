24ef79ea6586b55d7b4a77b8f88aea99f091e74b
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-18 02:37:57 +0530

    Separate client and server

---
+++ Server/models/exampleModel.js
@@ -0,0 +1,23 @@
+// models/exampleModel.js
+import mongoose from 'mongoose';
+
+// Define the schema for the example model
+const exampleSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true, // Adding a unique field
+        },
+        description: String,
+    },
+    {
+        timestamps: true, // Adding timestamps
+    },
+);
+
+// Create the example model from the schema
+const ExampleModel = mongoose.model('Example', exampleSchema);
+
+// Export the example model
+module.exports = ExampleModel;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+// import mongoose from 'mongoose';
+
+// MongoDB connection URI
+const MONGODB_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Your further code here
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 const app = express();
+const port = process.env.PORT;
 
 app.get('/test', (req, res) => {
     res.json('Server is working!');
 });
 
-app.listen(4000);
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+});
